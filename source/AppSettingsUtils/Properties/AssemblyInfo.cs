d27e4add34c261a4fc47e13f2a30e347ec9455a9
Daniel Wagner <daniel@wagners.name>
2016-10-15 13:27:46 +0200

    Fixed AssemblyVersion mess and bumped version to 1.0.1.0
    Signed-off-by: Daniel Wagner <daniel@wagners.name>

--- source/AppSettingsUtils/Properties/AssemblyInfo.cs
+++ source/AppSettingsUtils/Properties/AssemblyInfo.cs
@@ -17,16 +17,3 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 
 [assembly: Guid("d32c1422-6a7c-432e-aa8b-22149e540d76")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-
-[assembly: AssemblyVersion("1.0.*")]
--- source/SharedAssemblyInfo.cs
+++ source/SharedAssemblyInfo.cs
@@ -1,9 +1,14 @@
 using System.Reflection;
 
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
 #if DEBUG
 
 [assembly: AssemblyConfiguration("Debug")]
 #else
+
 [assembly: AssemblyConfiguration("Release")]
 #endif
 
@@ -12,4 +17,25 @@ using System.Reflection;
 [assembly: AssemblyCopyright("Copyright © 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyInformationalVersion("1.0.0.3")]
+
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+
+[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyInformationalVersion("1.0.1.0")]
+#if DEBUG
+
+[assembly: AssemblyVersion("1.0.*")]
+#else
+
+[assembly: AssemblyVersion("1.0.1.0")]
+#endif
